b33b1c6effef2b05f67b36a079d5b7a7b16416f8
Rafael Ribeiro <rafael.correia.poli@gmail.com>
2017-10-04 00:36:49 -0300

    First commit

--- src/index.js
+++ src/index.js
@@ -1,5 +1,38 @@
-import * as mySubmodule from './my-submodule'
+import toposort from '@rafa93br/toposort';
 
-export {
-  mySubmodule
-}
+const mapToGraph = config => Object.keys(config).map(name => ({
+  name,
+  edges: config[name][1],
+}));
+
+const getPromiseFromPromiseOrFn = (fn) => {
+  if (fn.then) return fn;
+  return () => Promise.resolve(fn());
+};
+
+export default map => ({
+  async start() {
+    const system = {};
+    const components = toposort(mapToGraph(map));
+    const stopFunctions = {};
+
+    for (const componentName of components) {
+      const componentFactory = map[componentName][0];
+      const component = componentFactory(system);
+      const componentInstance = await getPromiseFromPromiseOrFn(component.start)();
+      system[componentName] = componentInstance;
+      stopFunctions[componentName] = component.stop;
+    }
+    this.system = system;
+    this.stopFunctions = stopFunctions;
+    this.orderToStop = [...components].reverse();
+    return system;
+  },
+  async stop() {
+    for (const component of this.orderToStop) {
+      const stopFn = this.stopFunctions[component];
+      const stopPromise = getPromiseFromPromiseOrFn(stopFn);
+      await stopPromise();
+    }
+  },
+});
--- src/index.test.js
+++ src/index.test.js
@@ -1,6 +1,66 @@
-import { mySubmodule } from './'
+import systemMap from './';
 
-it('can use functions inside module', () => {
-	expect(mySubmodule.sum(1, 2)).toEqual(3)
-	expect(mySubmodule.multiply(2, 3)).toEqual(6)
-})
+const sleep = ms => new Promise(resolve => setTimeout(resolve, ms));
+
+describe('systemMap', () => {
+  jest.setTimeout(10000);
+  test.only('builds a system from components, starts it and stops', async () => {
+    const configCpStopFn = jest.fn();
+    const apolloCpStopFn = jest.fn();
+    const anotherCpStopFn = jest.fn();
+
+    const configCp = () => ({
+      async start() {
+        await sleep(100);
+        return {
+          env: 'prod',
+        };
+      },
+      stop() {
+        configCpStopFn();
+      },
+    });
+
+    const apolloCp = ({ config }) => ({
+      async start() {
+        await sleep(100);
+        return `something based on ${config.env}`;
+      },
+      async stop() {
+        await sleep(100);
+        apolloCpStopFn();
+      },
+    });
+
+    const anotherCp = ({ apollo, config }) => ({
+      start() {
+        return `apollo is ${apollo} and config is ${config.env}`;
+      },
+      stop() {
+        anotherCpStopFn();
+      },
+    });
+
+
+    const map = {
+      anotherCp: [anotherCp, ['apollo', 'config']],
+      apollo: [apolloCp, ['config']],
+      config: [configCp, []],
+    };
+
+    const systemControl = await systemMap(map);
+    const system = await systemControl.start();
+    expect(system).toEqual({
+      config: {
+        env: 'prod',
+      },
+      apollo: 'something based on prod',
+      anotherCp: 'apollo is something based on prod and config is prod',
+    });
+
+    await systemControl.stop();
+    expect(configCpStopFn.mock.calls.length).toBe(1);
+    expect(apolloCpStopFn.mock.calls.length).toBe(1);
+    expect(anotherCpStopFn.mock.calls.length).toBe(1);
+  });
+});
